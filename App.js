6b3cb4a3f326731f4a0eeb551b5c5c56f17b9ba0
Paweł Benkowski <pawlo97.pb@gmail.com>
2023-06-05 20:44:01 +0200

    Styling

--- App.js
+++ App.js
@@ -1,15 +1,37 @@
 import React from "react";
-import { Text, View, SafeAreaView } from "react-native";
+import { Text, View, SafeAreaView, StyleSheet } from "react-native";
 import SafeAreaViewAndroid from "./components/SafeAreaViewAndroid/SafeAreaViewAndroid";
 
 const App = () => {
   return (
     <SafeAreaView style={SafeAreaViewAndroid.safeAreaAndroid}>
-      <View>
-        <Text>Hello React Native</Text>
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello React Native</Text>
+        <Text style={[styles.text, styles.title]}>
+          Just pass an array to style to apply multiple styles
+        </Text>
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    margin: 10,
+    backgroundColor: "#1a2b3c",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#f5f5f5",
+  },
+  title: {
+    fontWeight: 700,
+  },
+});
 
 export default App;
