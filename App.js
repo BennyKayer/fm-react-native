fbe5263715b192cdbb0ba5fcffd05945580cd858
Paweł Benkowski <pawlo97.pb@gmail.com>
2023-06-05 20:19:45 +0200

    Basic React Native Components

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View, SafeAreaView } from "react-native";
+import SafeAreaViewAndroid from "./components/SafeAreaViewAndroid/SafeAreaViewAndroid";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={SafeAreaViewAndroid.safeAreaAndroid}>
+      <View>
+        <Text>Hello React Native</Text>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
